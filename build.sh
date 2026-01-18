656b4499d880bbc5eba0a81f49860932571b34f3
5eqn <subcat2077@gmail.com>
2026-01-18 10:19:44 +0800

    Use humble ROS2 DA3

---
+++ build.sh
@@ -0,0 +1,2 @@
+# Builds the base docker image
+docker build -f Dockerfile.base . --network=host -t depth_anything_3_ros2:base
--- init.sh
+++ init.sh
@@ -1,2 +1,1 @@
-# ros2 run rmw_zenoh_cpp rmw_zenohd &
-ros2 run ros1_bridge dynamic_bridge --bridge-all-topics
+ros2 run ros1_bridge parameter_bridge
